a91f3e82a4b6769e0495dd9a1df3c35c79ac1b3b
iLikeTrioxin <iLike135Trioxane@protonmail.com>
2021-10-08 20:29:19 +0200

    .

--- main.py
+++ main.py
@@ -5,22 +5,14 @@ import discord
 import nekos
 from discord.ext import commands
 from random import randrange
-from threading import Lock
-import asyncio
 
-# defines token for discord
-import credentials.py
+# defines TOKEN for discord
+from credentials import TOKEN
 
 bot = commands.Bot(command_prefix='#')
 
 cClients = []
 tags = ['feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo', 'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk', 'ngif', 'tickle', 'lewd', 'feed', 'gecg', 'eroyuri', 'eron', 'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'kemonomimi', 'nsfw_avatar', 'gasm', 'poke', 'anal', 'slap', 'hentai', 'avatar', 'erofeet', 'holo', 'keta', 'blowjob', 'pussy', 'tits', 'holoero', 'lizard', 'pussy_jpg', 'pwankg', 'classic', 'kuni', 'waifu', 'pat', '8ball', 'kiss', 'femdom', 'neko', 'spank', 'cuddle', 'erok', 'fox_girl', 'boobs', 'random_hentai_gif', 'smallboobs', 'hug', 'ero', 'smug', 'goose', 'baka', 'woof']
-
-resultImg = []
-async def getImg(tag):
-    img = nekos.img(tag)
-    with lock:
-        resultImg.append(img)
 
 @bot.command(name='wojciechowski')
 async def wojciechowski(ctx):
@@ -43,7 +35,14 @@ async def do(ctx, msg):
         for mem in connected.channel.members:
             print(mem.nick)
         print("done")
-        
+
+bulas = [['https://cdn.discordapp.com/attachments/752997029021745172/896085639047577660/20747972_1842000032795410_503458318229837137_o.png','Dobrakowska says jes to that idea'],['https://media.discordapp.net/attachments/755531580721594460/896087649184538634/image0.jpg?width=346&height=646', 'Kociuga feels this too'], ['https://media.discordapp.net/attachments/755531580721594460/896087700191465472/image2.jpg?width=346&height=646', 'Cowboy said jes']]
+@bot.command(name='bula')
+async def bula(ctx):
+    bulaid = randrange(0, len(bulas))
+    await ctx.send( bulas[bulaid][0] )
+    await ctx.send( bulas[bulaid][1] )
+
 @bot.command(name='ale')
 async def xd(ctx, msg=" "):
     omsg = "ale co?"
@@ -53,47 +52,21 @@ async def xd(ctx, msg=" "):
         
     await ctx.send(omsg)
 
-@bot.command(name='i')
-async def i(ctx, tag='random', x="1"):
-    print("requested " + tag)
+@bot.command(name='gimme')
+async def i(ctx, tag='random'):
     tag = tag.lower()
     
-    random = (tag == "random")
+    if(tag == "random"):
+    	tag = tags[randrange(0, len(tags))]
     
-    if (int(x) > 100):
-        ctx.send("max number of messeges per command is 100.")
-        return
+    if(tag not in tags):
+    	await ctx.send("This tag is not available.")
+    	return;
     
-    tasks = []
-    
-    if (random):
-        tasks = [nekos.img(tags[ randrange(0, len(tags) - 1) ]) for i in range(0, int(x))]
-    else:
-        tasks = [nekos.img(tag) for i in range(0, int(x))]
-    print("requested "+str(int(x))+" images.")
-    #await asyncio.gather(*tasks)
-    #print("generated "+str(len(resultImg))+" images.")
-    resultImg = tasks
-    msgs = [""]
-    cMsg = 0
-    for img in resultImg:
-        print("msg lenght = "+str(len(msgs[cMsg]) + len(img) + 1))
-        if((len(msgs[cMsg]) + len(img) + 3) >= 250):
-            print("created new msg")
-            msgs.append("")
-            cMsg += 1
-        msgs[cMsg] += (' ' + img)
-    
-    tasks = [ ctx.send( msg ) for msg in msgs ]
-    
-    asyncio.gather(*tasks)
-
+    await ctx.send(nekos.img(tag))
 
 @bot.command(name='h')
-async def h(ctx, tag='neko'):
-    print("requested "+tag)
-    #guild = ctx.guild
-    #existing_channel = discord.utils.get(guild.channels, name=channel_name)
+async def h(ctx):
     msg = ""
     msg += "For now avalible tags are:\n"
     
